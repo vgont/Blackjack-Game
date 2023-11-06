f8ea76adc66f8186a1369c739a1ca876470f5ae8
vgont <vgontijo34@gmail.com>
2023-11-05 23:17:46 -0300

    back finalized

--- server/server.py
+++ server/server.py
@@ -3,14 +3,13 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 import oracledb as odb
 
-
 # ----Oracle Database-----
 
 
 def getConnection():
     try:
         con = odb.connect(user="user", password="pass",
-                          host="oracle.fiap.com.br", service_name="orcl")
+                          dsn="oracle.fiap.com.br/orcl")
     except Exception as e:
         print(e)
     return con
@@ -20,27 +19,13 @@ def closeConnection(con):
     return con.close()
 
 
-def commit():
-    try:
-        print('abrindo')
-        conn = getConnection()
-        cursor = conn.cursor()
-        sql = "commit"
-        cursor.execute(sql)
-    except Exception as e:
-        print(e)
-    finally:
-        print('commitando/fechando')
-        closeConnection(conn)
-
-
-def insert_new_match(sum_cards_winner, sum_cards_loser, winner, winner_cards_drawed, total_cards_drawed):
-    sql = f'insert into t_cppy_match values (null, \'{sum_cards_winner}\', \'{sum_cards_loser}\', \'{winner}\', \'{winner_cards_drawed}\',\'{total_cards_drawed}\')'
+def insert_new_match(sum_cards_winner, sum_cards_loser, winner, winner_cards_drawed, loser_cards_drawed):
+    sql = f"insert into t_cppy_match values (null, {sum_cards_winner}, {sum_cards_loser}, '{winner}', {winner_cards_drawed}, {loser_cards_drawed})"
     try:
         conn = getConnection()
         cursor = conn.cursor()
         cursor.execute(sql)
-        commit()
+        conn.commit()
     except odb.exceptions.DatabaseError as e:
         print(f'Error: {e}')
     finally:
@@ -54,8 +39,8 @@ CORS(app)
 
 @app.route("/deck")
 def get_new_deck():
-    # deck_url = "https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1"
-    deck_url = "https://deckofcardsapi.com/api/deck/new/shuffle/?cards=AS,2S,AD,2D,AC,2C,AH,2H"
+    deck_url = "https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1"
+    # deck_url = "https://deckofcardsapi.com/api/deck/new/shuffle/?cards=AS,2S,AD,2D,AC,2C,AH,2H"
     try:
         response = requests.get(deck_url)
         deck = response.json()
@@ -79,10 +64,8 @@ def get_new_card(deck_id):
 def register_new_match():
     try:
         matchData = request.get_json()
-        # insert_new_match(sum_cards_winner=matchData['winnerCardsSum'], winner_cards_drawed=matchData['winnerCardsDrawed'], sum_cards_loser=matchData['loserCardsSum'],
-        #                  total_cards_drawed=matchData['totalCardsDrawed'], winner=matchData['winner'])
-        insert_new_match(sum_cards_loser=1, sum_cards_winner=1,
-                         total_cards_drawed=1, winner='player 1', winner_cards_drawed=1)
+        insert_new_match(sum_cards_loser=matchData['loserCardsSum'], sum_cards_winner=matchData['winnerCardsSum'],
+                         winner=matchData['winner'], winner_cards_drawed=matchData['winnerCardsDrawed'], loser_cards_drawed=matchData['loserCardsDrawed'])
         return jsonify({'message': 'Match registered'}), 201
     except Exception as e:
         print(f"Error: {e}")
