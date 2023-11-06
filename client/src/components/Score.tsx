d480dfcbb33acfcbfec021a9c3545b6a39c1d917
vgont <vgontijo34@gmail.com>
2023-11-06 08:35:55 -0300

    add draw counter

--- client/src/components/Score.tsx
+++ client/src/components/Score.tsx
@@ -6,12 +6,16 @@ interface IScore {
   handleDeck: () => void;
   scorePlayerOne: number;
   scorePlayerTwo: number;
+  totalMatchs: number;
+  totalDraws: number;
 }
 
 const Score: React.FC<IScore> = ({
   handleDeck,
   scorePlayerOne,
   scorePlayerTwo,
+  totalMatchs,
+  totalDraws,
 }) => {
   return (
     <div className="w-1/5 flex flex-col items-center bg-green-700 border-2 border-green-900 rounded-lg mx-2 p-3">
@@ -19,9 +23,8 @@ const Score: React.FC<IScore> = ({
       <div className="flex flex-col self-start ml-12 gap-4">
         <PlayerScore playerName="Player 1" score={scorePlayerOne} />
         <PlayerScore playerName="Player 2" score={scorePlayerTwo} />
-        <p className="text-white font-semibold text-2xl font-mono">{`Matchs: ${
-          scorePlayerOne + scorePlayerTwo
-        }`}</p>
+        <p className="text-white font-semibold text-2xl font-mono">{`Draws: ${totalDraws}`}</p>
+        <p className="text-white font-semibold text-2xl font-mono">{`Matchs: ${totalMatchs}`}</p>
       </div>
       <NewDeck onclick={handleDeck} />
     </div>
